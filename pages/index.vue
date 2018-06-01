bfdd96ae4f1d8be663950adee6a035e407d00a95
matthieudou <matthieu.doultremont@gmail.com>
2018-06-01 08:33:08 +0200

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        ivmovies
+      </h1>
+      <h2 class="subtitle">
+        IV Movies portfolio
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green">Documentation</a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey">GitHub</a>
+      </div>
+    </div>
+  </section>
+</template>
