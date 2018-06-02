9125ae5af6dbc26da067e128ab862fe6e7b074d4
matthieudou <matthieu.doultremont@gmail.com>
2018-06-02 22:01:11 +0200

    add projects page

---
+++ pages/projects/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <all-projects :projects="projects"></all-projects>
+</template>
+
+<script>
+import AllProjects from '@/components/projects/AllProjects'
+
+export default {
+  asyncData ({ app }) {
+    return app.$storyapi.get('cdn/stories', {version: 'draft', starts_with: 'projects/'})
+      .then(res => {
+        return {
+          projects: res.data.stories
+        }
+      })
+  },
+  components: {
+    AllProjects
+  }
+}
+</script>
