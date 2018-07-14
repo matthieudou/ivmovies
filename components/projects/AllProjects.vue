f19eeea58220c651c5d77f749712595276a50df3
matthieudou <matthieu.doultremont@gmail.com>
2018-07-14 14:19:25 +0200

    remove gradient on projects

--- components/projects/AllProjects.vue
+++ components/projects/AllProjects.vue
@@ -28,7 +28,7 @@ export default {
 }
 
 .content {
-  background-image: linear-gradient(to right bottom, rgba(0, 0, 0, .6), rgba(0, 0, 0, .4));
+  background-image: linear-gradient(to right bottom, rgba(0, 0, 0, .2), rgba(0, 0, 0, 0));
   transition: all .2s ease-in-out;
 
   &:hover {
