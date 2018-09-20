2d882f71e757aa53ab18091f0d7655b4756a2841
matthieudou <matthieu.doultremont@gmail.com>
2018-09-20 20:30:51 +0200

    little fixes on sentry plugin

--- plugins/sentry.js
+++ plugins/sentry.js
@@ -1,8 +1,8 @@
-import Vue from 'vue';
-import Raven from 'raven-js';
-import RavenVue from 'raven-js/plugins/vue';
+import Vue from 'vue'
+import Raven from 'raven-js'
+import RavenVue from 'raven-js/plugins/vue'
 
 Raven
-    .config('https://639cb43dc5df4b409a77859580cb375e@sentry.io/1223141')
-    .addPlugin(RavenVue, Vue)
-    .install();
+  .config('https://639cb43dc5df4b409a77859580cb375e@sentry.io/1223141')
+  .addPlugin(RavenVue, Vue)
+  .install()
